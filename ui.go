83250bf0140695a4173d146d200beddab307b09a
Maciej Pasternacki <maciej@3ofcoins.net>
2016-04-08 17:06:16 +0200

    wip

---
+++ ui.go
@@ -0,0 +1,230 @@
+package main
+
+import (
+	"strconv"
+	"unicode/utf8"
+
+	"github.com/nsf/termbox-go"
+)
+
+type UIState struct {
+	Desktops []WMDesktop
+	Selected int
+	Height   int
+	Width    int
+}
+
+func NewUIState(desks []WMDesktop) UIState {
+	st := UIState{
+		Desktops: desks,
+	}
+
+	for i, desk := range desks {
+		if desk.IsCurrent {
+			st.Selected = i
+		}
+		if nw := len(desk.Windows); nw > st.Height {
+			st.Height = nw
+		}
+		for _, win := range desk.Windows {
+			if nw := utf8.RuneCountInString(win.Name); nw > st.Width {
+				st.Width = nw
+			}
+		}
+	}
+
+	return st
+}
+
+func (ui *UIState) Desk() *WMDesktop {
+	return &ui.Desktops[ui.Selected]
+}
+
+func (ui *UIState) Prev() {
+	for cur := ui.Selected - 1; cur > 0; cur-- {
+		if ui.Desktops[cur].IsVisible() {
+			ui.Selected = cur
+			return
+		}
+	}
+}
+
+func (ui *UIState) Next() {
+	for cur := ui.Selected + 1; cur < len(ui.Desktops); cur++ {
+		if ui.Desktops[cur].IsVisible() {
+			ui.Selected = cur
+			return
+		}
+	}
+}
+
+var indexDigits = []rune{'⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹', '⁻', '⁼'}
+
+func (ui *UIState) Draw() {
+	cols, rows := termbox.Size()
+	fgFrame := termbox.ColorWhite | termbox.AttrBold
+
+	if rows < ui.Height+4 {
+		panic("Too little rows!")
+	}
+	if cols < ui.Width+2 {
+		panic("Too little cols!")
+	}
+
+	// Tab bar
+	termbox.SetCell(0, 2, '╭', fgFrame, termbox.ColorDefault)
+	col := 1
+	for i, desk := range ui.Desktops {
+		if !desk.IsVisible() {
+			continue
+		}
+
+		fg := termbox.ColorDefault
+		extra := termbox.Attribute(0)
+
+		if i == ui.Selected {
+			fg = termbox.ColorWhite
+		}
+
+		if desk.IsUrgent {
+			fg = termbox.ColorRed
+		}
+
+		if desk.IsCurrent {
+			extra = termbox.AttrUnderline
+		}
+
+		if i == ui.Selected {
+			extra = extra | termbox.AttrBold
+		}
+
+		if i == ui.Selected {
+			termbox.SetCell(col, 0, '╭', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 1, '│', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 2, '╯', fgFrame, termbox.ColorDefault)
+		} else {
+			termbox.SetCell(col, 0, ' ', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 1, ' ', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 2, '─', fgFrame, termbox.ColorDefault)
+		}
+		col++
+
+		// TODO: desktop # for keyboard shortcut here
+		index := ' '
+		if i < len(indexDigits) {
+			index = indexDigits[i]
+		}
+		if i == ui.Selected {
+			termbox.SetCell(col, 0, '─', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 1, index, termbox.ColorDefault, termbox.ColorDefault)
+			termbox.SetCell(col, 2, ' ', fgFrame, termbox.ColorDefault)
+		} else {
+			termbox.SetCell(col, 0, ' ', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 1, index, termbox.ColorDefault, termbox.ColorDefault)
+			termbox.SetCell(col, 2, '─', fgFrame, termbox.ColorDefault)
+		}
+		col++
+
+		for _, ch := range desk.Name {
+			termbox.SetCell(col, 1, ch, fg|extra, termbox.ColorDefault)
+			if i == ui.Selected {
+				termbox.SetCell(col, 0, '─', fgFrame, termbox.ColorDefault)
+				termbox.SetCell(col, 2, ' ', fgFrame, termbox.ColorDefault)
+			} else {
+				termbox.SetCell(col, 0, ' ', fgFrame, termbox.ColorDefault)
+				termbox.SetCell(col, 2, '─', fgFrame, termbox.ColorDefault)
+			}
+			col++
+		}
+
+		if i == ui.Selected {
+			termbox.SetCell(col, 0, '─', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 1, ' ', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 2, ' ', fgFrame, termbox.ColorDefault)
+		} else {
+			termbox.SetCell(col, 0, ' ', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 1, ' ', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 2, '─', fgFrame, termbox.ColorDefault)
+		}
+		col++
+
+		for _, ch := range strconv.Itoa(len(desk.Windows)) {
+			termbox.SetCell(col, 1, ch, termbox.ColorDefault, termbox.ColorDefault)
+			if i == ui.Selected {
+				termbox.SetCell(col, 0, '─', fgFrame, termbox.ColorDefault)
+				termbox.SetCell(col, 2, ' ', fgFrame, termbox.ColorDefault)
+			} else {
+				termbox.SetCell(col, 0, ' ', fgFrame, termbox.ColorDefault)
+				termbox.SetCell(col, 2, '─', fgFrame, termbox.ColorDefault)
+			}
+			col++
+		}
+
+		if i == ui.Selected {
+			termbox.SetCell(col, 0, '╮', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 1, '│', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 2, '╰', fgFrame, termbox.ColorDefault)
+		} else {
+			termbox.SetCell(col, 0, ' ', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 1, ' ', fgFrame, termbox.ColorDefault)
+			termbox.SetCell(col, 2, '─', fgFrame, termbox.ColorDefault)
+		}
+		col++
+	}
+
+	if col > ui.Width {
+		ui.Width = col
+	}
+
+	for ; col < ui.Width+1; col++ {
+		termbox.SetCell(col, 2, '─', fgFrame, termbox.ColorDefault)
+	}
+	termbox.SetCell(ui.Width+1, 2, '╮', fgFrame, termbox.ColorDefault)
+
+	// Window List
+	desk := ui.Desk()
+	for i, win := range desk.Windows {
+		fg := termbox.ColorDefault
+		extra := termbox.Attribute(0)
+
+		if win.IsUrgent {
+			fg = termbox.ColorRed
+		}
+
+		if win.IsActive {
+			extra = termbox.AttrUnderline
+		}
+
+		if i == desk.Selected {
+			fg = fg | termbox.AttrReverse
+		}
+
+		termbox.SetCell(0, i+3, '│', fgFrame, termbox.ColorDefault)
+		col = 1
+		for _, ch := range win.Name {
+			termbox.SetCell(col, i+3, ch, fg|extra, termbox.ColorDefault)
+			col++
+		}
+
+		for ; col < ui.Width+1; col++ {
+			termbox.SetCell(col, i+3, ' ', fg, termbox.ColorDefault)
+		}
+		termbox.SetCell(ui.Width+1, i+3, '│', fgFrame, termbox.ColorDefault)
+	}
+
+	for i := len(desk.Windows); i < ui.Height; i++ {
+		termbox.SetCell(0, i+3, '│', fgFrame, termbox.ColorDefault)
+		for j := 1; j < ui.Width+1; j++ {
+			termbox.SetCell(j, i+3, ' ', termbox.ColorDefault, termbox.ColorDefault)
+		}
+		termbox.SetCell(ui.Width+1, i+3, '│', fgFrame, termbox.ColorDefault)
+	}
+
+	termbox.SetCell(0, ui.Height+3, '╰', fgFrame, termbox.ColorDefault)
+	for j := 1; j < ui.Width+1; j++ {
+		termbox.SetCell(j, ui.Height+3, '─', termbox.ColorDefault, termbox.ColorDefault)
+	}
+	termbox.SetCell(ui.Width+1, ui.Height+3, '╯', fgFrame, termbox.ColorDefault)
+
+	termbox.Flush()
+}
---
+++ wm.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/BurntSushi/xgb/xproto"
+	"github.com/BurntSushi/xgbutil"
+	"github.com/BurntSushi/xgbutil/ewmh"
+	"github.com/BurntSushi/xgbutil/icccm"
+)
+
+type WMWindow struct {
+	XWin     xproto.Window
+	IsActive bool
+	IsUrgent bool
+	Name     string
+}
+
+type WMDesktop struct {
+	Number    uint
+	Name      string
+	Selected  int
+	IsCurrent bool
+	IsUrgent  bool
+	Windows   []WMWindow
+}
+
+func (wmw WMWindow) String() string {
+	activeFlag := ""
+	if wmw.IsActive {
+		activeFlag = "*"
+	}
+
+	urgentFlag := ""
+	if wmw.IsUrgent {
+		urgentFlag = "!"
+	}
+
+	return fmt.Sprintf("%v%v%#v", activeFlag, urgentFlag, wmw.Name)
+}
+
+func (wmd WMDesktop) String() string {
+	currentFlag := ""
+	if wmd.IsCurrent {
+		currentFlag = "*"
+	}
+
+	urgentFlag := ""
+	if wmd.IsUrgent {
+		urgentFlag = "!"
+	}
+
+	wins := make([]string, len(wmd.Windows))
+	for i, wmw := range wmd.Windows {
+		wins[i] = wmw.String()
+	}
+
+	return fmt.Sprintf("%s%s%d%v", currentFlag, urgentFlag, wmd.Number, wins)
+}
+
+func (desk *WMDesktop) IsVisible() bool {
+	return len(desk.Windows) > 0 || desk.IsCurrent
+}
+
+func (desk *WMDesktop) Next() {
+	if desk.Selected < len(desk.Windows)-1 {
+		desk.Selected++
+	}
+}
+
+func (desk *WMDesktop) NextWrap() {
+	if desk.Selected < len(desk.Windows)-1 {
+		desk.Selected++
+	} else {
+		desk.Selected = 0
+	}
+}
+
+func (desk *WMDesktop) Prev() {
+	if desk.Selected > 0 {
+		desk.Selected--
+	}
+}
+
+func (desk *WMDesktop) PrevWrap() {
+	if desk.Selected > 0 {
+		desk.Selected--
+	} else {
+		desk.Selected = len(desk.Windows) - 1
+	}
+}
+
+func Desktops(xu *xgbutil.XUtil) ([]WMDesktop, error) {
+	ndesk, err := ewmh.NumberOfDesktopsGet(xu)
+	if err != nil {
+		return nil, err
+	}
+
+	desktops := make([]WMDesktop, ndesk)
+
+	names, err := ewmh.DesktopNamesGet(xu)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range desktops {
+		desktops[i].Number = uint(i)
+		desktops[i].Name = names[i]
+	}
+
+	cdesk, err := ewmh.CurrentDesktopGet(xu)
+	if err != nil {
+		return nil, err
+	}
+	desktops[cdesk].IsCurrent = true
+
+	aw, err := ewmh.ActiveWindowGet(xu)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: check whether ClientListStackingGet is supported (cwm
+	// doesn't support it)
+	xws, err := ewmh.ClientListGet(xu)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, xw := range xws {
+		name, err := ewmh.WmNameGet(xu, xw)
+		if err != nil {
+			return nil, err
+		}
+
+		hints, err := icccm.WmHintsGet(xu, xw)
+		if err != nil {
+			return nil, err
+		}
+
+		desk, err := ewmh.WmDesktopGet(xu, xw)
+		if err != nil {
+			return nil, err
+		}
+
+		isActive := xw == aw
+		isUrgent := hints.Flags&icccm.HintUrgency == icccm.HintUrgency
+
+		desktops[desk].Windows = append(desktops[desk].Windows, WMWindow{
+			XWin:     xw,
+			IsActive: isActive,
+			IsUrgent: isUrgent,
+			Name:     name,
+		})
+
+		desktops[desk].IsUrgent = desktops[desk].IsUrgent || isUrgent
+
+		if isActive {
+			desktops[desk].Selected = len(desktops[desk].Windows) - 1
+		}
+	}
+
+	return desktops, nil
+}
